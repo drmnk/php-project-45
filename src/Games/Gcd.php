513cd74ec5b987f864e76f1bc9d8b2bcf81febcb
Denis Romanenko <deromanenko@gmail.com>
2022-12-11 15:00:48 +0300

    GCD game

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,30 +2,26 @@
 
 namespace BrainGames\Games\Gcd;
 
-function gameName()
-{
-    return 'Greatest Common Divisor';
-}
+use function BrainGames\Engine\startGame;
 
-// Функция "описать игру"
-function describeGame()
-{
-    return 'Find the Greates Common Divider for two numbers';
-}
+const TASK = 'Find the greatest common divisor of given numbers.';
 
 function gcd($a, $b)
 {
     return ($a % $b) ? gcd($b, $a % $b) : abs($b);
 }
 
-// Функция "поставить условие"
-// Возвращает массив с условием задачи и правильным ответом
-function createQuestionAndAnswer()
-{
-    $number1 = rand(1, 100);
-    $number2 = rand(1, 100);
-    $question = "What is GCD for numbers {$number1} and {$number2}";
-    $answer = gcd($number1, $number2);
 
-    return ['question' => $question, 'answer' => $answer];
+function startGcdGame()
+{
+    $gameData = function () {
+        $number1 = rand(1, 100);
+        $number2 = rand(1, 100);
+        $question = "{$number1} {$number2}";
+        $answer = gcd($number1, $number2);
+    
+        return ['question' => $question, 'answer' => (string) $answer];
+    };
+
+    startGame($gameData, TASK);    
 }
